bc12a61f35b6464d9044be30011d045615df1cee
Evan Kaloudis <evan@nyxmail.com>
2017-07-24 14:22:39 -0400

    Give the people what they want

--- src/404.js
+++ src/404.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import Kramer from './images/kramer.gif';
+import { NavLink } from 'react-router-dom';
 import './App.css';
 
 class NotFound extends Component {
-  render() {
-    return (
-      <div>
-        404: Page not found.
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                404: Page not found. <NavLink to="/">Head home.</NavLink>
+                <img className="Kramer-404" src={Kramer} alt="Kramer" />
+            </div>
+        );
+    }
 }
 
 export default NotFound;
