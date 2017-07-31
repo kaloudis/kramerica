286db2bf5b3d34a25b9357fee96f3e842f5e7304
Evan Kaloudis <evan@nyxmail.com>
2017-07-31 19:14:58 -0400

    Add an exact to our home NavLink on 404

--- src/404.js
+++ src/404.js
@@ -7,7 +7,7 @@ class NotFound extends Component {
     render() {
         return (
             <div>
-                404: Page not found. <NavLink to="/">Head home.</NavLink>
+                404: Page not found. <NavLink exact to="/">Head home.</NavLink>
                 <img className="Kramer-404" src={Kramer} alt="Kramer" />
             </div>
         );
